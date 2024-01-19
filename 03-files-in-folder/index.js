6d4304e22705757d63cee8f9c18daa830f3295fc
Oxana Savianok <oxanas8833@gmail.com>
2024-01-19 09:25:46 +0300

    feat: start a solution to the 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const fs = require('fs');
+const pathToFolder = path.join(__dirname, 'secret-folder');
+
+fs.readdir(pathToFolder, {withFileTypes: true}, (err, files) => {
+    if (err) throw err;
+    for (let file of files) {
+        if (file.isFile()) {
+            fs.stat(pathToFolder, file.name, (err, stats) => {
+                if (err) throw err;
+                //console.log(path.parse(file.name).name);
+                //console.log(path.extname(file.name).substring(1));
+                //console.log(stats.size);
+                console.log(path.parse(file.name).name + ' - ' + path.extname(file.name).substring(1) + ' - ' + stats.size + 'b');
+            })
+        } 
+    }
+})
