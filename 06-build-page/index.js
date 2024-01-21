f4ea334d252d535dfbb00229858d642688c4841f
Oxana Savianok <oxanas8833@gmail.com>
2024-01-21 19:43:48 +0300

    feat: add create html file in the solution of 06-build-page

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -3,17 +3,52 @@ const fs = require('fs');
 
 const pathToFolder = path.join(__dirname, 'project-dist');
 const pathToSourceFolder = path.join(__dirname, 'styles');
-const pathToFile = fs.createWriteStream(path.join(__dirname, 'project-dist', 'style.css'));
+const pathToTemplate = path.join(__dirname, 'template.html');
 
-//css
-fs.mkdir(pathToFolder, () => {
-fs.readdir(pathToSourceFolder, {withFileTypes: true}, (err, files) => {
-    if (err) throw err;
-    for (let file of files) {
-        if (path.extname(file.name) === '.css') {
-            const pathToSourceFile = fs.createReadStream(path.join(__dirname, 'styles', file.name), 'utf-8');
-            pathToSourceFile.on('data', chunk => pathToFile.write(chunk));
-        }
+
+//html
+
+
+fs.mkdir(pathToFolder, {withFileTypes: true}, () => {
+    //html
+    const readableStream = fs.createReadStream(pathToTemplate, 'utf-8');
+
+    function buildHtml() {
+        const pathToFileHtml = fs.createWriteStream(path.join(pathToFolder,'index.html'));
+        readableStream.on('data', chunk => {
+            let text = '';
+            const addTag = function(chunk) {    
+                text = chunk;   
+                const startIndex = text.indexOf('{{');
+                const endIndex = text.indexOf('}}');
+                const tag = text.slice(startIndex + 2, endIndex);        
+                const readableStream = fs.createReadStream(path.join(__dirname, 'components', tag + '.html'));
+                let component = '';
+                readableStream.on('data', (chunk) => {         
+                    component = component + chunk.toString();
+                    text = text.slice(0, startIndex) + component + text.slice(endIndex + 2);
+                    if(text.indexOf('}}') + 1) {
+                        addTag(text);
+                    } else {
+                        pathToFileHtml.write(text);
+                    }
+                });            
+            };
+            addTag(chunk);
+        });
     }
-})
+    
+    buildHtml();
+
+    //css
+    const pathToFile = fs.createWriteStream(path.join(__dirname, 'project-dist', 'style.css'));
+    fs.readdir(pathToSourceFolder, {withFileTypes: true}, (err, files) => {
+        if (err) throw err;
+        for (let file of files) {
+            if (path.extname(file.name) === '.css') {
+                const pathToSourceFile = fs.createReadStream(path.join(__dirname, 'styles', file.name), 'utf-8');
+                pathToSourceFile.on('data', chunk => pathToFile.write(chunk));
+            }
+        }
+    })
 })
